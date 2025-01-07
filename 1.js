14f3b4040bc60cabe477e68a3032d12d5435be9e
Sahithya19 <vemula.sanjini@gmail.com>
2025-01-07 13:14:09 +0530

    first commit

---
+++ 1.js
@@ -0,0 +1,29 @@
+//program
+var a = "Hello";
+let b = "hi";
+const c = 1234;
+
+console.log("Var declaration:",a);
+console.log("let declaration:",b);
+console.log("const declaration:",c);
+
+
+a = "xyz";
+b="abc";
+
+
+console.log("after reassignment:",a);
+console.log("after reassignment:",b);
+
+
+//task
+
+
+var myNum = 42;               
+let myStr = "Hello, world!";   
+let myBool = true;             
+
+
+console.log("Number:", myNum); 
+console.log("String:", myStr);  
+console.log("Boolean:", myBool);  
